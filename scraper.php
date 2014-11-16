9fe4a37648e3f72170a6641bf94d12678d1aa9ea
arasabbasi <aras.abbasi@googlemail.com>
2014-11-16 12:49:22 +0100

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,26 +1,105 @@
-<?
-// This is a template for a PHP scraper on Morph (https://morph.io)
-// including some code snippets below that you should find helpful
+<?php
 
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
-//
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-//
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
-// // Write out to the sqlite database using scraperwiki library
-// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
-//
-// // An arbitrary query against the database
-// scraperwiki::select("* from data where 'name'='peter'")
+//set_time_limit(0);
+//error_reporting(-1);
 
-// You don't have to do things with the ScraperWiki library. You can use whatever is installed
-// on Morph for PHP (See https://github.com/openaustralia/morph-docker-php) and all that matters
-// is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
-// has at least a table called data.
-?>
+	$lettersArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','XYZ'];
+
+	$pathToOverviewByLetter = 'http://service-bw.de/zfinder-bw-web/authorities.do?action=search&letter=';
+	$pathToResult 		= 'http://service-bw.de/zfinder-bw-web/authorities.do?bhid=';
+	$pathToEntry		= 'http://service-bw.de/zfinder-bw-web/authorities.do?beid=';
+
+	$bhidpattern = '/bhid=([0-9]*)&/mg';
+	$beidpattern = '/beid=([0-9]*)&/mg';
+
+	
+	$matches;
+	/*
+	foreach ($lettersArray as $value) {
+		print ('Verarbeite Buchstaben ' . $value . "\n");
+		ripBhidByLetter($value);
+	}
+	*/
+	ripBhidByLetter("C");
+
+function ripBhidByLetter($letter){
+	
+	$matchesBhid;
+	$output = scraperwiki::scrape("$pathToEntry" . $bhid);
+        preg_match_all($bhidpattern, $output, $matchesBhid);
+        
+        foreach ($matchesBhid[1] as $value){
+        	ripBeidByBhid ($value);
+        }
+}
+
+function ripBeidByBhid($bhid) {
+	$matchesBeid;
+	$output = scraperwiki::scrape("$pathToEntry" . $bhid);
+        preg_match_all($beidpattern, $output, $matchesBeid);
+        
+        foreach ($matchesBhid[1] as $value){
+        	ripByBeid ($value);
+        }
+	
+}
+
+
+
+function ripByBeid ($beid){
+	
+	$namepattern = '/<h1>(.*)<\/h1>/smiU';
+	$faxpattern = '/<td>Fax:<\/td><td>(.*)<\/td>/smiU';
+	$telefonpattern = '/<td>Telefon:<\/td><td>(.*)<\/td>/smiU';
+	$emailpattern = '/<td>E-Mail:<\/td><td><a href="(.*)"/imUs';
+	$emailpattern2 = '/href="mailto:(.*)">.*<\/a>/imUs'
+	$adresspattern1 = '/<h3>Hausanschrift:<\/h3><div class="column in_content"><p>(.*)<\/p><\/div>/smiU';
+	$adresspattern2 = '/<h3>Postanschrift:<\/h3><p>(.*)<\/p><table>/smiU';
+	$wwwpattern = '/<td>Homepage:<\/td><td><a href="(.*)"/smiU';
+
+	$output = scraperwiki::scrape("$pathToEntry" . $beid);
+	$output = mb_convert_encoding($output, 'UTF-8', mb_detect_encoding($output, 'UTF-8, ISO-8859-1', true));
+	    
+        preg_match($namepattern, $output, $temp);
+        $name = (isset($temp[1])) ? str_replace(';', ',',trim(preg_replace('/\s+/', ' ', $temp[1]))) : '';
+        
+        preg_match($faxpattern, $output, $temp);
+        $fax = (isset($temp[1])) ? trim(preg_replace('/\s+/', ' ', $temp[1])) : '';
+        
+        preg_match($telefonpattern, $output, $temp);
+        $telefon = (isset($temp[1])) ? trim(preg_replace('/\s+/', ' ', $temp[1])) : '';
+        
+        preg_match($emailpattern, $output, $temp);
+	$tempData =  html_entity_decode($temp[1]);
+        $email =  (isset($temp[1])) ? trim(preg_replace('/\s+/', ' ', $tempData)) : '';
+	if (strlen(trim($email)) == 0) {
+		preg_match($emailpattern2, $output, $temp);
+		$tempData =  html_entity_decode($temp[1]);
+       		$email =  (isset($temp[1])) ? trim(preg_replace('/\s+/', ' ', $tempData)) : '';
+	}
+        
+        preg_match($adresspattern1, $output, $temp);
+        $adress1 = (isset($temp[1])) ? str_replace(';',',',trim(preg_replace('/\s+/', ' ', $temp[1]))) : '';
+        $adress1 = str_ireplace('<br />', ',', $adress1);
+	$adress1 = strip_tags($adress1);
+
+        preg_match($adresspattern2, $output, $temp);
+        $adress2 = (isset($temp[1])) ? str_replace(';',',',trim(preg_replace('/\s+/', ' ', $temp[1]))) : '';
+        $adress2 = str_ireplace('<br />', ',', $adress2);
+	$adress2 = strip_tags($adress2);
+	
+	$adress = (isset($temp[1])) ? $adress2 : $adress1;
+
+
+        preg_match($wwwpattern, $output, $temp);
+        $url = (isset($temp[1])) ? trim(preg_replace('/\s+/', ' ', $temp[1])) : '';
+
+	$phonestring = (strlen(trim($telefon)) != 0) ? 'Telefon: ' . trim($telefon) : '';
+	$faxstring = (strlen(trim($fax)) != 0) ? 'Fax: ' . trim($fax) : '';
+	$contactconnector = (strlen($phonestring) > 0 && strlen($faxstring) > 0) ? ', ': '';
+	$contact = $phonestring . $contactconnector . $faxstring;
+	$juristication_slug = 'sachsen-anhalt';
+	
+	scraperwiki::save_sqlite(array('data'), array('name' => $name,'email' => $email, 'address' => $adress, 'contact' => $contact, 'jurisdiction__slug' => $jurisdiction__slug, 'other_names' => '', 'description' => '', 'topic__slug' => '', 'parent__name' => '', 'classification' => '', 'url' => $url, 'website_dump' => '', 'request_note' => $beid));
+      	print $name . "\n";
+}
